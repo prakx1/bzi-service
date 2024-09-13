a9d8e79cfa1929cbe7c9d6a2c719617d98efcdf7
prakx1 <prakx2050@gmail.com>
2024-09-14 00:15:22 +0530

    requiremet.txt

--- app.py
+++ app.py
@@ -45,5 +45,5 @@ def analyze_stock():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
